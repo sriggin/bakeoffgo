39f9063d5e22eeadbf7aa1e39694811991bea472
Sean Riggin <sriggin@gmail.com>
2022-07-28 12:07:16 -0600

    Getting gRPC server running, disabling catboost until I can fix it

--- README.md
+++ README.md
@@ -9,3 +9,13 @@ brew install protobuf
 go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.28
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2
 ```
+
+See setup instructions for [catboost-go](https://github.com/bourbaki/catboost-go), and customize:
+```shell
+# you probably need to customize this
+export CATBOOST_REPO=~/dev/thirdparty/catboost
+export CATBOOST_DIR="$CATBOOST_REPO/catboost/libs/model_interface"
+export C_INCLUDE_PATH=$CATBOOST_DIR:$C_INCLUDE_PATH
+export LIBRARY_PATH=$CATBOOST_DIR:$LIBRARY_PATH
+export LD_LIBRARY_PATH=$CATBOOST_DIR:$LD_LIBRARY_PATH
+```
--- main.go
+++ main.go
@@ -4,7 +4,8 @@ import (
 	ts "bakeoffgo/trafficscanner"
 	"context"
 	"fmt"
-	"github.com/bourbaki/catboost-go"
+	"google.golang.org/grpc/reflection"
+
 	"google.golang.org/grpc"
 	"log"
 	"net"
@@ -16,7 +17,7 @@ var (
 
 type trafficScannerServer struct {
 	ts.UnimplementedTrafficScannerServer
-	model *catboost.Model
+	//model *catboost.Model
 }
 
 func (s *trafficScannerServer) IsTrafficValid(ctx context.Context, in *ts.TrafficScanRequest) (*ts.TrafficScanResponse, error) {
@@ -28,12 +29,14 @@ func (s *trafficScannerServer) IsTrafficValid(ctx context.Context, in *ts.Traffi
 }
 
 func main() {
-	model, err := catboost.LoadFullModelFromFile("model.cbm")
-	if err != nil {
-		log.Fatalf("Failed to load model: %v", err)
-	}
+	/*
+		model, err := catboost.LoadFullModelFromFile("model.cbm")
+		if err != nil {
+			log.Fatalf("Failed to load model: %v", err)
+		}
+	*/
 
-	server := trafficScannerServer{model: model}
+	server := trafficScannerServer{}
 
 	listen, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", port))
 	if err != nil {
@@ -42,7 +45,9 @@ func main() {
 
 	grpcServer := grpc.NewServer()
 	ts.RegisterTrafficScannerServer(grpcServer, &server)
-
+	// register reflection service on gRPC server
+	reflection.Register(grpcServer)
+	log.Printf("TrafficScanner gRPC service listening on port %d", port)
 	err = grpcServer.Serve(listen)
 	if err != nil {
 		log.Fatalf("Failed to serve TrafficScanner service on listener: %v", err)
